7a332e13ac3d36ac6aac260a2f185ebaf728b19e
Samija Kujrakovic <skujrakovi1@etf.unsa.ba>
2021-07-26 10:34:10 +0200

    Implemented add weapon method in weapon controller

--- Startup.cs
+++ Startup.cs
@@ -18,6 +18,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using Swashbuckle.AspNetCore.Filters;
 using Microsoft.AspNetCore.Http;
+using dotnet_rpg.Services.WeaponService;
 
 namespace dotnet_rpg
 {
@@ -64,6 +65,7 @@ namespace dotnet_rpg
                     };
                 });
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped<IWeaponService, WeaponService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
